e98be47e2692e149363ff5c5c13f1816e7fd331a
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2023-08-23 02:16:20 -0400

    Time: 5426 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,5 +1,5 @@
 WITH CTE1 as (
-  SELECT u.name, dense_rank() OVER (order by count(m.movie_id) desc, u.name asc) as rank1
+  SELECT u.name, dense_rank() OVER (ORDER BY COUNT(m.movie_id) DESC, u.name ASC) AS rank1
   FROM MovieRating m
   JOIN Users u ON u.user_id = m.user_id
   GROUP BY u.user_id
@@ -7,11 +7,11 @@ WITH CTE1 as (
 
 CTE2 as
 (
-Select m2.title, dense_rank() over(order by avg(rating) desc, m2.title asc) as rank2 
-from MovieRating m1
+SELECT m2.title, dense_rank() OVER (ORDER BY AVG(rating) DESC, m2.title ASC) AS rank2 
+FROM MovieRating m1
 JOIN Movies m2
 ON m1.movie_id = m2.movie_id
-where m1.created_at between '2020-02-01' and '2020-02-29'
+WHERE m1.created_at between '2020-02-01' and '2020-02-29'
 GROUP BY m1.movie_id
 )
 
