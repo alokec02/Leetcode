16a17e4c6c6e6fa19045366732c0818a750f3d0c
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2024-10-31 14:20:35 -0400

    Time: 857 ms (84%), Space: 25 MB (42.86%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -6,27 +6,35 @@ class Solution:
         R = len(robot)
         F = len(factory)
         INF = 10 ** 20
-
+        
+        @cache
+        # rindex -> 0 to R
+        # findex -> 0 to F
+        # each input takes O(R^2)
+        # O(R^3 * F) time
         def get_min(rindex, findex):
             if rindex == R:
                 return 0
+            
             if findex == F:
                 return INF
-
+                
             x, limit = factory[findex]
-
+            
+            # no take (take no robots)
             best = get_min(rindex, findex + 1)
-
+            cost = 0
+            
+            # take i robots
             for i in range(1, limit + 1):
                 if rindex + i > R:
                     break
-                cost = 0
-                for j in range(rindex, rindex + i):
-                    cost += abs(x - robot[j])
+
+                cost += abs(x - robot[rindex + i - 1])
                 best = min(best, get_min(rindex + i, findex + 1) + cost)
             
             return best
-        
-        return get_min(0,0)
+            
+        return get_min(0, 0)
 
         
