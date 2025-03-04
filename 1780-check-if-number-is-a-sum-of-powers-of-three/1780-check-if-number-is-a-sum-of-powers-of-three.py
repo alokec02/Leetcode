26aa75beba1968ba076c829b6d7425fd490de721
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2025-03-04 17:16:38 -0500

    Time: 259 ms (5.49%), Space: 17.7 MB (59.61%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        def backtrack(i, cur):
+            if cur == n:
+                return True
+            if cur > n or 3**i > n:
+                return False
+
+            if backtrack(i + 1, cur + 3**i):
+                return True
+            return backtrack(i + 1, cur)
+
+        return backtrack(0, 0)
