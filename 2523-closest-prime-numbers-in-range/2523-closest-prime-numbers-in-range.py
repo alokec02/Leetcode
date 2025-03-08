a497d4707405380e28658f4421dd993072d4fdde
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2025-03-07 19:48:01 -0500

    Time: 4611 ms (10.22%), Space: 35.1 MB (26.52%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -6,11 +6,10 @@ class Solution:
             is_prime = [True] * (right + 1)
             is_prime[0] = is_prime[1] = False
 
-            for n in range(2, int(sqrt(right)) + 1):
-                if not is_prime[n]:
-                    continue
+            for n in range(2, int(sqrt(right))+1):
                 for m in range(n + n, right + 1, n):
                     is_prime[m] = False
+        
             primes = []
             for i in range(len(is_prime)):
                 if is_prime[i] and i >= left:
@@ -19,9 +18,9 @@ class Solution:
 
         primes = get_primes()
         res = [-1, -1]
-        diff = right - left + 1    
+        diff = right - left + 1
         for i in range(1, len(primes)):
             if primes[i] - primes[i - 1] < diff:
                 diff = primes[i] - primes[i - 1]
-                res = [primes[i-1], primes[i]]
+                res = [primes[i - 1], primes[i]]
         return res
